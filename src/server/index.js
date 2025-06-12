940097673c85e8984884697a80b3373d7c5bee55
Marcelo Perez Izco <marceloperezizco@gmail.com>
2025-06-12 21:08:21 +0100

    feat(server): serve static files from dist

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("dist"));
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
