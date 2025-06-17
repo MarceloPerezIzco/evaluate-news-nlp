457130e626e36369031ecd4a1ef367a124d49cb5
Marcelo Perez Izco <marceloperezizco@gmail.com>
2025-06-17 20:44:04 +0100

    feat(server): serve static files from dist and add start script

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "../..dist")));
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`✅ Server running on http://localhost:${PORT}`);
 });
